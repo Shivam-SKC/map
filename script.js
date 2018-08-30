891ca59da49af8e0861e5ead7bf2efc3be0a2c09
mafiadeveloper <42809430+mafiadeveloper@users.noreply.github.com>
2018-08-30 11:48:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var divide=document.querySelector('.divide');
+var bottom=document.querySelector('#bottom');
+
+divide.addEventListener('click',function(){
+    
+
+     divide.style.height='200px';
+});
